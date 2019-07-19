35cc1c69885ac8bb4610b194fca2fee87da5610f
bryvl <bryan.val93@gmail.com>
2019-07-18 23:07:24 -0400

    had incorrect paths for some components

--- client/src/views/NoMatch.js
+++ client/src/views/NoMatch.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Col, Row, Container } from "../components/Grid";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
 function NoMatch() {
   return (
